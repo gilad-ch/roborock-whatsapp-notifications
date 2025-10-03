f5f7461ea19270f81ccdb371fe410d0115f8f4c4
gilad-ch <gcohenhav@gmail.com>
2025-10-03 18:09:02 +0300

    improved WhahApi session management

--- src/utils/roborockApi.py
+++ src/utils/roborockApi.py
@@ -8,10 +8,11 @@ load_dotenv()
 username = os.getenv("ROBOROCK_USERNAME")
 password = os.getenv("ROBOROCK_PASSWORD")
 
+
 class RoborockApi:
     def __init__(self):
         self.mqtt_client = None
-    
+
     async def start(self):
         web_api = RoborockApiClient(username=username)
         user_data = await web_api.pass_login(password=password)
@@ -20,24 +21,26 @@ class RoborockApi:
         product_info = {product.id: product for product in home_data.products}
         device_data = DeviceData(device, product_info[device.product_id].model)
         self.mqtt_client = RoborockMqttClientV1(user_data, device_data)
-    
+
     async def __aenter__(self):
         await self.start()
         return self
+
     async def __aexit__(self, exc_type, exc_value, traceback):
         pass
 
     def _parse_status(self, status):
         dock_error = status.dock_error_status
-        return{
+        return {
             "water_shortage": dock_error == RoborockDockErrorCode.water_empty,
-            "dirty_tank_missing": dock_error == RoborockDockErrorCode.dirty_tank_latch_open,
-            "waste_water_tank_full": dock_error == RoborockDockErrorCode.waste_water_tank_full,
-            "basin_blocked": dock_error == RoborockDockErrorCode.cleaning_tank_full_or_blocked,
+            "dirty_tank_missing": dock_error
+            == RoborockDockErrorCode.dirty_tank_latch_open,
+            "waste_water_tank_full": dock_error
+            == RoborockDockErrorCode.waste_water_tank_full,
+            "basin_blocked": dock_error
+            == RoborockDockErrorCode.cleaning_tank_full_or_blocked,
         }
-    
+
     async def get_status(self):
         status = await self.mqtt_client.get_status()
         return self._parse_status(status)
-
-
--- src/utils/whahApi.py
+++ src/utils/whahApi.py
@@ -1,8 +1,13 @@
+import logging
+import time
+from pyrate_limiter import wraps
 import requests
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
+logger = logging.getLogger(__name__)
+
 base_url = os.getenv("WHAH_BASE_URL")
 session_name = (
     os.getenv("WHAH_SESSION_NAME") if os.getenv("WHAH_SESSION_NAME") else "default"
@@ -12,8 +17,35 @@ if not (base_url or session_name):
     raise ValueError("WHAH_BASE_URL or WHAH_SESSION_NAME is not set")
 
 
+def ensure_session(func):
+    """Decorator to make sure WAHA session is running before the function call."""
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        WhahApi._start_session()
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
 class WhahApi:
     @staticmethod
+    def _start_session():
+        session_info_url = f"{base_url}/api/sessions/{session_name}"
+        try:
+            response = requests.get(session_info_url)
+            if response.json().get("status") != "WORKING":
+                logger.info("WHAHA Session stopped, starting WAHA session...")
+                start_url = f"{base_url}/api/sessions/{session_name}/start"
+                response = requests.post(start_url)
+                time.sleep(20)
+                response.raise_for_status()
+        except requests.exceptions.RequestException as e:
+            logger.error(f"Error starting session: {e}")
+            raise e
+
+    @staticmethod
+    @ensure_session
     def send_message(message: str, number: int):
         chat_id = f"{number}@c.us"
         url = f"{base_url}/api/sendText"
@@ -30,10 +62,11 @@ class WhahApi:
             return response.json()
 
         except requests.exceptions.RequestException as e:
-            print(f"Error sending message: {e}")
+            logger.error(f"Error sending message: {e}")
             raise e
 
     @staticmethod
+    @ensure_session
     def send_group_message(message: str, group_id: str):
         chat_id = f"{group_id}@g.us"
         url = f"{base_url}/api/sendText"
@@ -50,5 +83,5 @@ class WhahApi:
             return response.json()
 
         except requests.exceptions.RequestException as e:
-            print(f"Error sending message: {e}")
+            logger.error(f"Error sending message: {e}")
             raise e
